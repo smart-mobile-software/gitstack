d8695c3a402ea216ead78e415f687debce81ba66
poiuytrez <contact@gitstack.com>
2012-03-28 07:07:21 -0700

    Apache is now embed in GitStack. No need for an installer anymore. Closes #40

---
+++ apache/cgi-bin/printenv.pl
@@ -0,0 +1,13 @@
+#!D:/programs/perl/bin/perl.exe
+##
+##  printenv -- demo CGI program which just prints its environment
+##
+
+print "Content-type: text/plain; charset=iso-8859-1\n\n";
+foreach $var (sort(keys(%ENV))) {
+    $val = $ENV{$var};
+    $val =~ s|\n|\\n|g;
+    $val =~ s|"|\\"|g;
+    print "${var}=\"${val}\"\n";
+}
+
