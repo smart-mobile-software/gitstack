fbac133b405c9608b7a90cdeb88627ef11ccd39f
poiuytrez <contact@gitstack.com>
2012-04-24 03:10:45 -0700

    Static file path modif

--- app/app/settings.py
+++ app/app/settings.py
@@ -56,7 +56,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = 'C:/dev/gitstack/app/staticfiles/'
+STATIC_ROOT = 'C:/dev/gitstack/app/app/staticfiles/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
