db4e7f32617fbbc44ba15e9d778e30e7430fdf58
poiuytrez <contact@gitstack.com>
2012-05-18 04:26:38 -0700

    gitphp auth. Closes #80

--- gitphp/include/Authentication.class.php
+++ gitphp/include/Authentication.class.php
@@ -2,7 +2,6 @@
 /**
  * GitPHP Authentication
  *
- * Logging class
  *
  * @author Smart Mobile Software
  * @package GitPHP
@@ -45,6 +44,7 @@ class GitPHP_Authentication
 			}
 			else
 			{
+				
 				// The user should be authenticated
 				// Ask for username/password
 				if (!isset($_SERVER['PHP_AUTH_USER'])) {
@@ -55,19 +55,24 @@ class GitPHP_Authentication
 					exit;
 				} else {
 					// try to authenticate
+					$authenticated = false;
 					$username = $_SERVER['PHP_AUTH_USER'];
 					$password = $_SERVER['PHP_AUTH_PW'];
-					$authenticated = false;
-					$authMethod = $this->getAuthMethod();
-					// authenticate with ldap or by file
-					if($authMethod == "file"){
-						$authenticated = $this->authenticateFile($username, $password);
-					}
-					if ($authenticated == false){
-						header('WWW-Authenticate: Basic realm="Please enter your git repository credentials"');
-						header('HTTP/1.0 401 Unauthorized');
-						echo 'Your GitStack credentials were not entered correcly. Please ask your GitStack administrator to give you a username/password and give you access to this repository.';
-						exit;
+					// Check if the user is in the array of read users
+					if(in_array($username, $users)){
+						$authMethod = $this->getAuthMethod();
+						// authenticate with ldap or by file
+						if($authMethod == "file"){
+							$authenticated = $this->authenticateFile($username, $password);
+						} if($authMethod == "ldap") {
+							$authenticated = $this->authenticateLdap($username, $password);
+
+						}
+						if ($authenticated == false){
+							$this->denyAuthentication();
+						}
+					} else {
+						$this->denyAuthentication();
 					}
 					
 				}
@@ -110,7 +115,7 @@ class GitPHP_Authentication
 		$settingsDir = GitPHP_Config::GetInstance()->GetValue('gitstacksettings', '');
 
 		// read the ini file
-		$ini_array = parse_ini_file($settingsDir, true);
+		$ini_array = parse_ini_file($settingsDir, true, INI_SCANNER_RAW);
 		$authMethod = $ini_array['authentication']['authmethod'];
 		// should contain "ldap" or "file"
 		return $authMethod;
@@ -168,6 +173,98 @@ class GitPHP_Authentication
 		
 	}
 	
+	// authenticate a user with a file
+	private function authenticateLdap($username, $password){
+		$authenticated = false;
+		
+		// Read the gitstack settings file
+		$settingsDir = GitPHP_Config::GetInstance()->GetValue('gitstacksettings', '');
+
+		// read the ini file
+		$ini_array = parse_ini_file($settingsDir, true, INI_SCANNER_RAW);
+		$ldapParams = $ini_array['authentication'];
+		
+		$ldapServerUrl = $ldapParams['ldapprotocol'] . "://" . $ldapParams['ldaphost'] . ":" . $ldapParams['ldapport'] . "/";
 	
+			
+		if ($this->checkldapuser($username, $password, $ldapServerUrl, $ldapParams ['ldapbasedn'],$ldapParams ['ldapattribute'], $ldapParams ['ldapbinddn'],$ldapParams ['ldapbindpassword'],$ldapParams ['ldapfilter']  )) {
+			$authenticated = true;
+		} else {
+			$authenticated = false;
+		} 
+			
+
+		
+		return $authenticated;
+		
+		
+	}
 	
+	private function checkLdapUser($username,$password,$ldap_server, $base_dn, $attribute, $bind_dn, $bind_password, $filter){
+		if($connect=@ldap_connect($ldap_server)){ // if connected to ldap server
+
+		ldap_set_option($connect, LDAP_OPT_PROTOCOL_VERSION, 3);
+		
+		
+
+		// bind to ldap connection
+		if(($bind=@ldap_bind($connect, $bind_dn, $bind_password)) == false){
+		  print "bind:__FAILED__<br>\n";
+		  return false;
+		}
+		
+		// setup the filter
+		$filter="(&" . $filter . "(" . $attribute . "=" . $username . "))";  // single filter
+
+
+		// search for user
+		if (($res_id = ldap_search( $connect,
+									$base_dn,
+									$filter)) == false) {
+		  print "failure: search in LDAP-tree failed<br>";
+		  return false;
+		}
+		
+
+		if (ldap_count_entries($connect, $res_id) != 1) {
+		  print "failure: username $username found more than once<br>\n";
+		  return false;
+		}
+
+		if (( $entry_id = ldap_first_entry($connect, $res_id))== false) {
+		  print "failur: entry of searchresult couln't be fetched<br>\n";
+		  return false;
+		}
+
+		if (( $user_dn = ldap_get_dn($connect, $entry_id)) == false) {
+		  print "failure: user-dn coulnd't be fetched<br>\n";
+		  return false;
+		}
+
+		/* Authentifizierung des User */
+		if (($link_id = ldap_bind($connect, $user_dn, $password)) == false) {
+		  print "failure: username, password didn't match: $user_dn<br>\n";
+		  return false;
+		}
+
+		return true;
+		@ldap_close($connect);
+	  } else {                                  // no conection to ldap server
+		echo "no connection to '$ldap_server'<br>\n";
+	  }
+
+	  echo "failed: ".ldap_error($connect)."<BR>\n";
+
+	  @ldap_close($connect);
+	  return(false);
+
+	}
+	
+	private function denyAuthentication(){
+		header('WWW-Authenticate: Basic realm="Please enter your git repository credentials"');
+		header('HTTP/1.0 401 Unauthorized');
+		echo 'Your GitStack credentials were not entered correcly. Please ask your GitStack administrator to give you a username/password and give you access to this repository.';
+		exit;
+	}
+
 }
